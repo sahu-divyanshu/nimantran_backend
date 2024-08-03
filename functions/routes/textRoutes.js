9f61b2a9dd447d07e84e6e4407eb0ad7d3df59e7
divyanshu_sahu <divyanshusahucoc@gmail.com>
2024-08-03 14:01:36 +0530

    state

---
+++ functions/routes/textRoutes.js
@@ -0,0 +1,7 @@
+const { saveText } = require("../controllers/textController");
+const { authenticateJWT } = require("../middleware/auth");
+
+const router = require("express").Router();
+
+router.post("/texts/save",authenticateJWT,saveText)
+
